f9b82b64620f0981a4cd52f38b9f523063befbc2
praveenkej <85007428+praveenkej@users.noreply.github.com>
2023-12-10 16:18:35 +0530

    Add files via upload

---
+++ Meme Bazaar/filter.js
@@ -0,0 +1,781 @@
+const data = [
+  {
+    id: 1,
+    name: "Miracle Miracle",
+    img: "img/miracle.jpg",
+    cat: "Akshay Kumar",
+  },
+  {
+    id: 11,
+    name: "Control Uday Control",
+    img: "img/control.png",
+ 
+    cat: "Welcome",
+  },
+  {
+    id: 2,
+    name: "Bas Kar Bhai",
+    img: "img/baas.jpg",
+   
+    cat: "Other",
+  },
+  {
+    id: 3,
+    name: "Mera Hi Maal Chura Kar ",
+    img: "img/saale.jpg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 4,
+    name: "Taste Kha Hai",
+    img: "img/taste.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Pyaari Samajh Gyi",
+    img: "img/pyaari.jpg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Ashleel Hai Ye launda",
+    img: "img/ashleel.jpg",
+    
+    cat: "3 Idiots",
+  },
+ 
+  {
+    id: 5,
+    name: "Me Expert Hu",
+    img: "img/expert.png" ,
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Ye Bhi Thik Hai ",
+    img: "img/yebhi.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Itni khushi Mujhe",
+    img: "img/itni.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Wo Raat Apun Do Baje",
+    img: "img/2bje.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Dono Alag Alag Hote Hai Kya",
+    img: "img/dono.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Neeche Se Check Kar ",
+    img: "img/nichey.jpg",
+    
+    cat: "3 Idiots",
+  },
+
+  {
+    id: 5,
+    name: "Kya Kru Me Mar Jau",
+    img: "img/kyakru.jpg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Samajh Nhi Aaya Par ",
+    img: "img/samajh.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Aapke Paas 30 Sec Hai",
+    img: "img/apke.jpg",
+    
+    cat: "3 Idiots",
+  },
+  {
+    id: 5,
+    name: "Mujhe Ghar Jaana Hai",
+    img: "img/ghar.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Abba Nhi Manenge ",
+    img: "img/abba.jpg",
+    
+    cat: "3 Idiots",
+  },
+  {
+    id: 5,
+    name: "Beta Tumse Na Ho Payega",
+    img: "img/beta.png",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Mera Toh Itna Life Kharab Ho",
+    img: "img/life.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Seh Lenge Thoda",
+    img: "img/seh.png",
+    
+    cat: "Welcome",
+  },
+  {
+    id: 5,
+    name: "Haan Ye Krlo Pehle",
+    img: "img/haan.jpg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "O Bhai Maaro Mujhe Maaro ",
+    img: "img/maro.jpg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Mujhe Kyo Toda",
+    img: "img/toda.jpg",
+    
+    cat: "Welcome",
+  },
+  {
+    id: 5,
+    name: "Paisa Laya",
+    img: "img/paisa.jpg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Abhi Maza Aayega Na",
+    img: "img/mza.jpg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Badi Achhi Baat Ki",
+    img: "img/bdi.png",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Dekh Rha Hai Binod ",
+    img: "https://indian.memetemplates.in/uploads/1675096256.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Jal Lijiye",
+    img: "https://indian.memetemplates.in/uploads/1642460335.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Bhai Kya Kar Rha Hai Tu ",
+    img: "https://indian.memetemplates.in/uploads/1675005298.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "It's Not Funny",
+    img: "https://indian.memetemplates.in/uploads/1675352238.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Chhoti Bacchi Ho Kya",
+    img: "https://indian.memetemplates.in/uploads/1676051197.jpeg",
+    
+    cat: "Bollywood",
+  },
+
+  {
+    id: 5,
+    name: "Saala Ye Dhukh",
+    img: "https://indian.memetemplates.in/uploads/1676393448.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Tere Baap Ki Saadi ",
+    img: "https://indian.memetemplates.in/uploads/1676479333.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Jalwa Hai Humara",
+    img: "https://indian.memetemplates.in/uploads/1677415388.jpeg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Ye Badhiya Tha Guru ",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ye-badhiya-tha-guru-1200x674.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Aapse Better Umeed Kiye The",
+    img: "https://images.assettype.com/nationalherald/2021-06/bea94fe5-f339-4cec-8df6-e4463ab68927/Sehwag_s_Mirzapur_meme_after_India_loss_in_WTC_final_goes_viral.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Buddhi Bahut Tez Hai Tumhari",
+    img: "https://bharatiyavishwa.com/wp-content/uploads/2020/06/buddhi-bohut-tej-hai-tumhari.jpg",
+    
+    cat: "Mirzapur",
+  },
+
+
+
+
+
+  {
+    id: 5,
+    name: "Arey Band Kar... Band Kar  ",
+    img: "https://www.india.com/wp-content/uploads/2022/09/taarak-mehta-ooltah-chashmah-memes.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Goli Beta Masti Nhi",
+    img: "https://indianmemetemplates.com/wp-content/uploads/goli-beta-masti-nahi.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Nahane Ja Nahane",
+    img: "https://memetemplates.in/uploads/1628243703.jpeg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Kya Tapleek Hai Aapko",
+    img: "https://memes.co.in/memes/update/uploads/2021/02/kya-tapleek-hai-aap-ko.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Happy Diwali",
+    img: "https://humornama.com/wp-content/uploads/2021/11/Happy-Diwali-Meme-on-Jethalal-608x608.jpg",
+    
+    cat: "Taarak Mehta",
+  },
+  {
+    id: 5,
+    name: "Chabi Kha Hai",
+    img: "https://indianmemetemplates.com/wp-content/uploads/chabi-kaha-hai-1200x675.jpg",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Gaadi Start Kar",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ae-gaadi-start-kar-1200x675.jpg",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Saanp Ko Paal Rha Tha",
+    img: "https://indianmemetemplates.com/wp-content/uploads/saala-saanp-ko-paal-raha-tha.jpg",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Achha Baat Nhi Hai",
+    img: "https://indianmemetemplates.com/wp-content/uploads/accha-baat-nahi-hai-ye-1200x674.jpg",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  { id: 5,
+    name: "Akshay Kumar Swap",
+    img:"https://indianmemetemplates.com/wp-content/uploads/akshay-kumar-swap-ajnabee.jpg",
+    
+    cat: "Akshay Kumar",
+   
+  },
+  {
+    id: 5,
+    name: "Aapka Hai Toj Kaagaz Dikhaiye",
+    img: "https://indianmemetemplates.com/wp-content/uploads/aapka-hai-toh-kagaz-dikhaiye.jpg",
+    
+    cat: "Gangs of Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Bhai Paisa Ho Toh Kya ",
+    img: "https://memes.co.in/memes/update/uploads/2021/12/kbidhnd3wj671-950x500.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Akshay Kumar Smile",
+    img: "https://memes.co.in/Uploads/Media/Nov22/Mon14/774/88104d.jpg",
+    
+    cat: "Akshay Kumar",
+  },
+
+  {
+    id: 5,
+    name: "Are Mujhe Chakkar Aane lga",
+    img: "https://allmemetemplates.files.wordpress.com/2020/02/arey-mujhe-chakkar-aane-laga-hai-768x432-1.jpg?w=768",
+    
+    cat: "Akshay Kumar",
+  },
+ 
+
+  {
+    id: 5,
+    name: "Jor Jor Se Bolke",
+    img: "https://www.scrolldroll.com/wp-content/uploads/2021/09/Akshay-Kumar-Meme-templates-14.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+ 
+  {
+    id: 5,
+    name: "Do Laana",
+    img: "https://akm-img-a-in.tosshub.com/sites/lallantop/wp-content/uploads/2022/03/whatsapp-image-2022-03-16-at-33908-pm_160322-100925.jpeg",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Ye Bhed Bhav Kyi",
+    img: "https://1.bp.blogspot.com/-ml9fQqX7uyg/XvynZyfDoxI/AAAAAAAADA4/gYDZwLuZS8EcxcJ41EdvOfMc2kHyXvvLgCLcBGAsYHQ/s1600/i710r.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+
+  {
+    id:5,
+    name:"Kaun Bhawk Rha Hai",
+    img:"https://allmemetemplates.files.wordpress.com/2020/02/img_20200709_183602.jpg?w=707",
+    cat:"Welcome",
+  },
+  {
+    id: 5,
+    name: "Saale Ko Bomb Se Uda Dunga",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrCjqZ6Qi20YguKFYm7cuW-G9fkAys_uOpIuo9EINjR-S1X9vEuBIcK4ueSxDgKgyCewg&usqp=CAU",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Thik Hai Bhai Me Chalta Hu ",
+    img: "https://indianmemetemplates.com/wp-content/uploads/thik-hai-bhai-ab-mai-chalta-hoon-1200x600.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Ye Kab Hua",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ye-kab-hua-1200x674.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id:5,
+    name:"Lagta Hai Aapke Pyaar",
+    img:"https://indian.memetemplates.in/uploads/1641576394.jpeg",
+    cat:"Welcome",
+  },
+
+  {
+    id:5,
+    name:"Ye Raaj Bhi Usi Ke Saath",
+    img:"https://d33wubrfki0l68.cloudfront.net/ecd056bce3e35d5e8a653a422d82b9635ef42b00/18588/img/templates/welcome-raaz.jpg",
+    cat:"Welcome",
+  },
+  {
+    id:5,
+    name:"Bolne De Takleef Hui Hai",
+    img:"https://getmemetemplates.com/wp-content/uploads/2020/04/PicsArt_04-15-11.35.58-1024x464.jpg",
+    cat:"Welcome",
+  },
+  {
+    id:5,
+    name:"Saala Jalta Hai Mujhse ",
+    img:"https://allmemetemplates.files.wordpress.com/2020/02/saala-jalta-hain-mujhse-768x382-1.jpg?w=768",
+    cat:"Welcome",
+  },
+  {
+    id:5,
+    name:"Bhai Sahab Ye Kis Line",
+    img:"https://www.scrolldroll.com/wp-content/uploads/2020/05/Welcome-Memes-9.jpg",
+    cat:"Welcome",
+  },
+
+
+
+
+  {
+    id:5,
+    name:"Kehna Kya Chahte HO",
+    img:"https://indian.memetemplates.in/uploads/1677405954.jpeg",
+    cat:"3 Idiots",
+  },
+  {
+    id:5,
+    name:"Hai Himmat Aayega",
+    img:"https://humornama.com/wp-content/uploads/2022/02/Hai-Himmat-Meme-Template-on-3-Idiots.jpg",
+    cat:"3 Idiots",
+  },
+  {
+    id:5,
+    name:"Permanent Hoon Sir",
+    img:"https://pbs.twimg.com/media/F6iENuQWsAAdkwE.jpg",
+    cat:"3 Idiots",
+  },
+  {
+    id:5,
+    name:"Saalary Kitna Loge",
+    img:"https://www.scrolldroll.com/wp-content/uploads/2020/06/3-idiots-memes-14.jpg",
+    cat:"3 Idiots",
+  },
+  {
+    id:5,
+    name:"Jhooth Bol Rha Hai",
+    img:"https://indianmemetemplates.com/wp-content/uploads/nahi-jhooth-bol-raha-hai.jpg",
+    cat:"3 Idiots",
+  },
+
+
+
+
+
+
+
+
+
+
+  {
+    id:5,
+    name:"Me Koi Gaon Se Aaya Hu Kya",
+    img:"https://memetemplatehouse.com/wp-content/uploads/2020/06/mein-koi-gaon-se-aya-hun-kya-Rajpal-Yadav-hera-pheri-1024x575.jpg",
+    cat:"Rajpal Yadav",
+  },
+  {
+    id:5,
+    name:"Aage Ka Kya Socha Hai",
+    img:"https://indianmemetemplates.com/wp-content/uploads/maalik-aage-ka-kya-socha-hai-1024x576.jpg",
+    cat:"Rajpal Yadav",
+  },
+  {
+    id:5,
+    name:"Humko Chahiye Full Izzat",
+    img:"https://humornama.com/wp-content/uploads/2020/12/Ab-Humko-Chahiye-Full-Izzat-meme-template-of-Mirzapur-608x342.jpg",
+    cat:"Mirzapur",
+  },
+  {
+    id:5,
+    name:"Aate Hi Kaam Suru",
+    img:"https://humornama.com/wp-content/uploads/2020/10/Aate-Hi-Kaam-Shuru-Kar-Diye-meme-template-of-Mirzapur.jpg",
+    cat:"Mirzapur",
+  },
+
+  {
+    id:5,
+    name:"Hum Karte Hai Prabhandh",
+    img:"https://memeheist.com/wp-content/uploads/2023/09/Hum-Karte-Hai-Prabandh-Meme-.jpg",
+    cat:"Mirzapur",
+  },
+  {
+    id:5,
+    name:"Bahut Takleef Hoti Hai",
+    img:"https://allmemetemplates.files.wordpress.com/2020/02/bohut-taqleef-hoti-hai-jab-aap-yogy-ho-aur-log-aapki-yogyata-na-pehchane-768x431-1.jpg",
+    cat:"Mirzapur",
+  },
+
+  {
+    id:5,
+    name:"Kaanp Kahe Rhi Ho ",
+    img:"https://www.memestemplates.com/wp-content/uploads/2020/08/kaanp-kahe-rahi-ho.jpg",
+    cat:"Gangs of Wasseypur",
+  },
+  {
+    id:5,
+    name:"Kya Kiye Koot Diye",
+    img:"https://indianmemetemplates.com/wp-content/uploads/kya-kiye-koot-diye.jpg",
+    cat:"Gangs of Wasseypur",
+  },
+  {
+    id:5,
+    name:"Sexy Lag Rha Tha",
+    img:"https://indianmemetemplates.com/wp-content/uploads/2019/01/aise-hi-sexy-lag-raha-tha.jpg",
+    cat:"Gangs of Wasseypur",
+  },
+
+
+  {
+    id: 5,
+    name: "Hum Koi Mandir Ka Ghanta",
+    img: "https://i0.wp.com/adthejokester.com/wp-content/uploads/2021/09/Hum-koi-mandir-ka-Ghanta-hai-kya-meme-template.jpg?resize=860%2C484&ssl=1",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Aurat Ka Chakkar Babu Bhaiya",
+    img: "https://indianmemetemplates.com/wp-content/uploads/Aurat-Ka-Chakkar-Babu-Bhaiya-800x600.jpg?crop=1",
+    
+    cat: "Akshay Kumar",
+  },
+  {
+    id: 5,
+    name: "Kya Maal Hai Yaar",
+    img: "https://i.pinimg.com/736x/5d/62/e6/5d62e63e30d893287e447b27596d13c5.jpg",
+    
+    cat: "Akshay Kumar",
+  },
+
+];
+
+const  showlightbox = (img) =>{
+  lightBox.querySelector("img").src=img;
+  lightBox.classList.add("show");
+}
+
+const hidelightBox = () => {
+  const lightBox = document.querySelector('.lightbox');
+  lightBox.classList.remove('show');
+    
+   location.reload();
+  
+  
+};
+
+
+const productsContainer = document.querySelector(".products");
+const searchInput = document.querySelector(".search");
+const lightBox= document.querySelector(".lightbox");
+const close= lightBox.querySelector(".clse");
+const categoriesContainer = document.querySelector(".cats");
+const displayProducts = (filteredProducts) => {
+  productsContainer.innerHTML = filteredProducts
+    .map(
+     
+
+      (product) =>
+        `
+   
+   <div class="product">
+       <div class="hoverimage">
+        <img onclick="showlightbox('${product.img}')"
+          src=${product.img}
+          alt=""
+          />
+          </div>
+    
+          <span  class="name">${product.name}</span>
+          <span class="categor">${product.cat}</span>
+          <button class="download" onclick="downloadImage('${product.img}')">Download Meme
+        </button>
+        </div>
+   
+    `
+    )
+    .join("");
+};
+ 
+
+const downloadImage = (imageUrl) => {
+  // Create an anchor element to trigger the download
+  const downloadLink = document.createElement('a');
+  downloadLink.href = imageUrl;
+  downloadLink.download = imageUrl.split('/').pop(); // Set the file name by extracting it from the URL
+  downloadLink.click();
+};
+
+const showTextOverlay = (imgSrc) => {
+  const mainImage = document.getElementById('mainImage');
+  const textOverlay = mainImage.parentElement.querySelector('.text-overlay');
+  mainImage.src = imgSrc;
+  textOverlay.style.display = 'block';
+};
+displayProducts(data);
+
+searchInput.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+
+  if (value) {
+    displayProducts(
+      data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+    );
+  } else {
+    displayProducts(data);
+  }
+});
+
+const setCategories = () => {
+  const allCats = data.map((item) => item.cat);
+  const categories = [
+    "All",
+    ...allCats.filter((item, i) => {
+      return allCats.indexOf(item) === i;
+    }),
+  ];
+
+  categoriesContainer.innerHTML = categories
+    .map(
+      (cat) =>
+        `
+      <span class="cat">${cat}</span>
+    `
+    )
+    .join("");
+
+  categoriesContainer.addEventListener("click", (e) => {
+    const selectedCat = e.target.textContent;
+
+    selectedCat === "All"
+      ? displayProducts(data)
+      : displayProducts(data.filter((item) => item.cat === selectedCat));
+  });
+};
+
+
+
+setCategories();
+close.addEventListener("click",hidelightBox)
+
+
+
+
+// Function to add text overlay to the image in the lightbox
+const addText = () => {
+  const textInput = document.getElementById('textInput');
+  const text = textInput.value;
+
+  // Get the image element in the lightbox
+  const lightboxImage = lightBox.querySelector('img');
+
+  // Create a new element to hold the text
+  const textElement = document.createElement('div');
+  textElement.classList.add('text-overlay');
+  textElement.innerText = text;
+ 
+  // Style the text overlay
+  textElement.style.position = 'absolute';
+  textElement.style.top = '33%'; // Adjust the position to center vertically
+  textElement.style.left = '50%'; // Adjust the position to center horizontally
+  textElement.style.transform = 'translate(-50%, -50%)'; 
+  textElement.style.color = 'yellow'; // Set text color
+  textElement.style.fontWeight = 'bold'; // Set font weight
+  textElement.style.fontSize = '24px'; // Set font size
+  textElement.style.textShadow = '2px 2px 5px rgba(0, 0, 0, 8)';
+  textElement.style.maxWidth = `${lightboxImage.width - 40}px`; 
+  textElement.style.wordWrap = 'break-word';
+textElement.style.backgroundColor='black';
+  // Append the text overlay to the image
+ 
+  lightboxImage.parentElement.appendChild(textElement);
+
+
+  const downloadButton = document.querySelector('.down');
+  downloadButton.addEventListener('click', () => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const lightboxImage = lightBox.querySelector('img');
+
+    canvas.width = lightboxImage.naturalWidth;
+    canvas.height = lightboxImage.naturalHeight;
+    context.drawImage(lightboxImage, 0, 0);
+
+    const textOverlays = document.querySelectorAll('.text-overlay');
+
+
+    
+    textOverlays.forEach((textOverlay) => {
+      // Get the computed styles of the text overlay
+      const computedStyles = window.getComputedStyle(textOverlay);
+      context.textAlign = computedStyles.textAlign;
+context.textBaseline = computedStyles.textBaseline;
+context.fillStyle = 'yellow';
+context.font = 'bold 30px sans-serif';
+context.backgroundColor = 'black';
+context.strokeStyle = 'black'; // Outline color
+context.lineWidth = 35; // Outline thickness
+context.lineJoin = 'round'; // Outline style
+      // Calculate the offset positions of the text relative to the image
+      const lightboxWidth = 1000; // Example: Replace with your actual lightbox width
+  const textX = (lightboxWidth - lightboxImage.naturalWidth) / 2 + lightboxImage.naturalWidth / 2;
+      const textY = 60;
+
+// Draw text at the adjusted positions on the canvas
+context.strokeText(textOverlay.innerText, textX, textY);
+context.fillText(textOverlay.innerText, textX, textY);
+    });
+
+    // Create a download link for the canvas image with the added text
+    const downloadLink = document.createElement('a');
+    downloadLink.href = canvas.toDataURL('image/png');
+    downloadLink.download = 'image_with_text.png';
+
+    // Trigger the download link
+    downloadLink.click();
+  });
+};
+
+// Event listener for the "Add Text" button
+const addTextButton = document.getElementById('addTextButton');
+addTextButton.addEventListener('click', addText);
+
+
+
+
+
+
+
+
+
+
+
